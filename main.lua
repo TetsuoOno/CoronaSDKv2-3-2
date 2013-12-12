d13c3461b30f9378de8295a0767b6258f73113a3
Tetsuo Ono <calmdesign.lifestyle@gmail.com>
2013-12-13 08:13:49 +0900

    CoronaSDK HandsOn(Vol.2) #3の資料その２

---
+++ main.lua
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------
+--CoronaSDK ver.1202までと、ver.2076からで表示時のオブジェクトの原点座標が異なります。
+----------------------------------------------------------------------------
+
+local rect = display.newRect(50, 50,  100, 100 )
+
+--[[
+local function moveRect( event ) 
+	if( event.phase == "ended" ) then
+		transition.to(rect, {time = 1000, y = 400, })--x = 250, rotation = 720
+	end
+end
+
+rect:addEventListener( "touch", moveRect )
+--]]
+
+local function complete()
+	local compText = display.newText("Complete", 0, 0, "HelveticaNeue-UltraLight", 60)
+	compText.x = display.contentWidth /2
+	compText.y = display.contentHeight /2
+	transition.to(compText, {delay = 500, time = 1000, alpha = 0})
+end
+
+local function jump()
+	transition.to(rect, {time = 1000, x = 100, y = 100, rotation = rect.rotation -360, onComplete = complete })
+end
+
+local function step( )
+	transition.to(rect, {time = 1000, x = 250, onComplete = jump})
+end
+
+local function hop( event )
+	if( event.phase == "ended" ) then
+		transition.to(rect, {time = 1000, y = 400, onComplete = step})
+	end
+end
+
+rect:addEventListener( "touch", hop )
