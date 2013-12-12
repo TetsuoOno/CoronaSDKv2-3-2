944b47f705e4b2fbf97194f1ea6bfa011229265d
Tetsuo Ono <calmdesign.lifestyle@gmail.com>
2013-12-13 08:34:28 +0900

    functionを追加

--- main.lua
+++ main.lua
@@ -14,11 +14,17 @@ end
 rect:addEventListener( "touch", moveRect )
 --]]
 
+local function changeColor()
+	--rect:setFillColor(math.random(0,255), math.random(0,255), math.random(0,255), 255)
+	rect:setFillColor(math.random(0,1), math.random(0,1), math.random(0,1), 1)
+end
+
 local function complete()
 	local compText = display.newText("Complete", 0, 0, "HelveticaNeue-UltraLight", 60)
 	compText.x = display.contentWidth /2
 	compText.y = display.contentHeight /2
 	transition.to(compText, {delay = 500, time = 1000, alpha = 0})
+	changeColor()
 end
 
 local function jump()
